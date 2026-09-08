710fee8c7966d23518ef6876d3bee75902236089
xingxingmofash <xingxingmofashu@outlook.com>
2026-09-08 16:28:46 +0800

    Bump version to 1.0.1

--- Wallpaper/CLI/Commands/VersionCommand.swift
+++ Wallpaper/CLI/Commands/VersionCommand.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum Version {
-    static let number = "1.0.0"
+    static let number = "1.0.1"
     static let name = "vw"
     static let bundleID = "com.example.vw"
     static let full = "\(name) \(number)"
